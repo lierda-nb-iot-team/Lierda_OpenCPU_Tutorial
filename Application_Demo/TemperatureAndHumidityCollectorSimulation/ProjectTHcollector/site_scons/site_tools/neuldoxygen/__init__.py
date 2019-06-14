62b5b135b36b1e0c01fcccf7d0dd1c754bf9e01f
harryliubg <17809213295@163.com>
2019-06-14 10:49:14 +0800

    update

--- Application_Demo/TemperatureAndHumidityCollectorSimulation/ProjectTHcollector/site_scons/site_tools/neuldoxygen/__init__.py
+++
@@ -1,7 +0,0 @@
-#===============================================================================
-# @brief    Scons make file
-# Copyright (c) 2016 NEUL LIMITED
-#===============================================================================
-
-from neuldoxygen import generate
-from neuldoxygen import exists
